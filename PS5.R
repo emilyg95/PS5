f47cf2cdc0af2395634bc35b00a78dfdc4f398db
Emily Garner <emilyg1995@yahoo.com>
2018-03-01 19:36:41 -0600

    Simpson's rule
    created a function that calculated the necessary interior values for simpson's rule (a, b, n, h) the same as trapezoidal but subsetted the y values further by not only removing the first and last ones but splitting them up by every other and multiplying the "odd" values by 4 and the "even" values by 2 as called for in the equation

--- PS5.R
+++ PS5.R
@@ -1,20 +1,34 @@
-x = 1:4 ## test vector of x values
-y = 1:4 ## test vector of y values
-sum(2*suby)
+z = 1:5 ## test vector of x values
+q = 1:5 ## test vector of y values
 
 Trap = function(x, y){ ## formula for trapezoidal rule that takes in arguments for x values and y values
   a = min(x) ## defines minimum value of x
   b = max(x) ## defines maximum value of x
   n = length(x) ## defines the number of elements in x
   h = ((b-a)/(n-1)) ## defines h as in trapezoidal rule (n-1 because a = x0 in the rule, not x1, so it should be a measure of the number of intervals not the number of points)
-  ay = min(y) ## defines minimum value of y
-  by = max(y) ## defines maximum value of y
-  remove = c(ay, by) ## creates a new vector of min and max y values
-  suby = setdiff(y, remove) ## creates a new vector of all y values except min and max
-  t = (h/2)*(ay + by + sum(2*suby)) ## calculates t as in the trapezoidal rule
+  a_y = min(y) ## defines minimum value of y
+  b_y = max(y) ## defines maximum value of y
+  remove = c(a_y, b_y) ## creates a new vector of min and max y values
+  sub_y = setdiff(y, remove) ## creates a new vector of all y values except min and max
+  t = (h/2)*(a_y + b_y + sum(2*sub_y)) ## calculates t as in the trapezoidal rule
   return(t) ## returns t
 }
 
-Trap(x, y) ## check
+Trap(q, z) ## check
 
+Simpson = function(x, y){ ## formula for simpson's rule that takes in arguments for x values and y values
+  a = min(x) ## defines minimum value of x
+  b = max(x) ## defines maximum value of x
+  n = length(x) ## defines the number of elements in x
+  h = ((b-a)/(n-1)) ## defines h as in trapezoidal rule (n-1 because a = x0 in the rule, not x1, so it should be a measure of the number of intervals not the number of points)
+  a_y = min(y) ## defines minimum value of y
+  b_y = max(y) ## defines maximum value of y
+  remove = c(a_y, b_y) ## creates a new vector of min and max y values
+  sub_y = setdiff(y, remove) ## creates a new vector of all y values except min and max
+  odd_y = seq(min(sub_y),max(sub_y),2) ## subsets the new vector of y values further into every other
+  even_y = seq(min(sub_y)+1,length(sub_y),2) ## takes the other half of the new vector of y values
+  s = (h/3)*(a_y + b_y + sum(4*odd_y) + sum(2*even_y)) ## calculates s as in simpson's rule
+  return(s)
+}
 
+Simpson(q,z) ## check
